6bdecd6f5dd1030a40fb27820461aca60c2bc2ff
gukhman <gukhman@gmail.com>
2024-10-31 14:46:44 +0200

    Новый проект

---
+++ app/src/main/java/com/example/a15_4_contacts/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a15_4_contacts
+
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupWindowInsets(R.id.main)
+        setupToolbar(R.id.toolbar, false)
+    }
+}
